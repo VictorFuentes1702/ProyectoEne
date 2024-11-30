00103dc1109d4fea037b7c0ed5eca6ac00404f99
VictorFuentes1702 <victor.f.vargas.2c@gmail.com>
2024-11-30 19:33:13 -0300

    1.2
    Nuevo formulario

---
+++ ProyectoENE/ProyectoENE/Entities/View/EjemploGit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoENE.Entities.View
+{
+    public partial class EjemploGit : Form
+    {
+        public EjemploGit()
+        {
+            InitializeComponent();
+        }
+    }
+}
